1026e6949f9ff78e291de45e09ab8d4a53927cbb
BMABHINAV <abhinavbm.cse2024@citchennai.net>
2026-02-14 17:04:09 +0530

    Remove duplicate New folder

--- New folder/README.md
+++
@@ -1,1 +0,0 @@
-djangoccp
--- New folder/library_ai/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LibraryAiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'library_ai'
